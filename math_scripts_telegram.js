c9846c2c9976fea6a33cc6faac32818b1def3e62
iamnewie <iamnewie@gmail.com>
2016-10-07 00:58:30 +0700

    Add files via upload

---
+++ math_scripts_telegram.js
@@ -0,0 +1,29 @@
+
+document.body.onkeyup = function(e){
+	if(e.keyCode === 32){
+		var arg1 = parseInt(document.getElementById("task_x").innerHTML);
+		var arg2 = parseInt(document.getElementById("task_y").innerHTML);
+		var op = document.getElementById("task_op").innerHTML;
+
+		var ans = 0;
+		var ans_screen = parseInt(document.getElementById("task_res").innerHTML);
+
+
+			switch(op){
+				case "+" : ans = arg1 + arg2; break;
+				case "–" : ans = arg1 - arg2; break;
+				case "/" : ans = arg1 / arg2; break;
+				case "×" : ans = arg1 * arg2; break;
+			}
+		if(ans_screen != ans){
+			document.getElementById("button_wrong").click();
+		}
+		else {
+			document.getElementById("button_correct").click();
+		}
+		console.log("arg1 = "+ arg1 + "\nop = " + op + "\narg2 = " + arg2 + "\nans = " + ans + "\nans_screen = " + ans_screen);
+	}
+};
+
+
+
